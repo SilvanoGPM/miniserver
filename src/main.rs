18d36f392d09ccd5d1f7e8f2b115c3ebccb8f52b
SkyG0D <silvanosilvino@hotmail.com>
2023-07-19 19:51:50 -0300

    feat: add not found to other uris

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,21 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("example.json").unwrap();
+    let is_index_page = request_line == "GET / HTTP/1.1";
+
+    let (status, content_type, filename) = if is_index_page {
+        ("200 OK", "application/json", "example.json")
+    } else {
+        ("404 NOT FOUND", "text/html", "404.html")
+    };
+
+    let status_line = format!("HTTP/1.1 {status}");
+    let content = fs::read_to_string(filename).unwrap();
     let content_length = content.len();
-    
-    let headers = format!("Content-Length: {content_length}, Content-Type: application/json")
+
+    let headers = format!("Content-Length: {content_length}, Content-Type: {content_type}")
         .replace(", ", "\r\n");
 
     let response = format!("{status_line}\r\n{headers}\r\n\r\n{content}");
